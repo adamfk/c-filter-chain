7a2360cfab2a95a5b3aa91ff34f0386ed2bbca34
Adam FK <adam.k@jcaelectronics.ca>
2018-03-17 23:34:43 -0500

    test: adapt for visual studio gtest runner

--- source/i32-filter-chain.gtest.cpp
+++ source/i32-filter-chain.gtest.cpp
@@ -1,9 +1,11 @@
 
 #include "i32-filter-chain.h"
-#include "i32-filter-chain.h" //note no problems doing this
-#include "i8-filter-chain.h"
 
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
 #include <stdio.h>
+
+//TODO create general macros file
 
 /**
 * Macro for getting the size of an array that is known at compile time. Code from Google's Chromium project.
@@ -13,19 +15,14 @@
 */
 #define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
 
-int main(void) {
 
+TEST(FilterChain_i32, PassthroughTest) {
 
   fcb32_PassThrough p32;
   fcb32_PassThrough_new(&p32);
 
-  fcb32_IirLowPass1 iir32;
-  fcb32_IirLowPass1_new(&iir32);
-  iir32.new_ratio = 0.20f;
-
   fc32_FilterChain filter_chain = { 0 };
   fcb32_GenericBlock* blocks[] = {
-    &iir32.block,
     &p32.block,
   };
 
@@ -33,15 +30,19 @@ int main(void) {
   filter_chain.block_count = COUNT_OF(blocks);
   fc32_FilterChain_setup(&filter_chain);
 
+
   for (size_t i = 0; i < 50; i++)
   {
-    int32_t output = fc32_FilterChain_filter(&filter_chain, 1000);
-    printf("step %i, output %d\n", i, output);
+    int32_t input = i;
+    int32_t output = fc32_FilterChain_filter(&filter_chain, input);
+    EXPECT_EQ(output, input);
   }
 
+}
 
 
-  fcb32_IirLowPass1 iir8;
-  fcb32_IirLowPass1_new(&iir8);
-
+int main(int argc, char** argv) {
+  ::testing::InitGoogleMock(&argc, argv);
+  int result = RUN_ALL_TESTS();
+  return result;
 }
