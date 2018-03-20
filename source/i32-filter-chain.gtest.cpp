fcd26ef732d3a35598fe6f73fdc4e7442de8fc10
Adam FK <adam.k@jcaelectronics.ca>
2018-03-19 21:46:07 -0500

    test: add IIR tests

--- source/i32-filter-chain.gtest.cpp
+++ source/i32-filter-chain.gtest.cpp
@@ -24,14 +24,12 @@ TEST(FilterChain_i32, OnePassthrough) {
   fcb32_PassThrough_new(&p);
 
   fc32_FilterChain filter_chain = { 0 };
-  fcb32_GenericBlock* blocks[] = {
+  fcb32_GenericBlock* filter_blocks[] = {
     &p.block,
   };
-
-  filter_chain.blocks = &blocks[0];
-  filter_chain.block_count = COUNT_OF(blocks);
+  filter_chain.blocks = &filter_blocks[0];
+  filter_chain.block_count = COUNT_OF(filter_blocks);
   fc32_FilterChain_setup(&filter_chain);
-
 
   for (size_t i = 0; i < 50; i++)
   {
@@ -56,7 +54,6 @@ TEST(FilterChain_i32, TwoPassthrough) {
     &p1.block,
     &p2.block,
   };
-
   filter_chain.blocks = &blocks[0];
   filter_chain.block_count = COUNT_OF(blocks);
   fc32_FilterChain_setup(&filter_chain);
@@ -67,6 +64,91 @@ TEST(FilterChain_i32, TwoPassthrough) {
     int32_t input = i;
     int32_t output = fc32_FilterChain_filter(&filter_chain, input);
     EXPECT_EQ(output, input);
+  }
+}
+
+
+TEST(FilterChain_i32, OneIirPrecalc) {
+
+  fcb32_IirLowPass1 iir1;
+  fcb32_IirLowPass1_new(&iir1);
+  iir1.new_ratio = 0.2f;
+
+
+  fc32_FilterChain filter_chain = { 0 };
+  fcb32_GenericBlock* blocks[] = {
+    &iir1.block,
+  };
+  filter_chain.blocks = &blocks[0];
+  filter_chain.block_count = COUNT_OF(blocks);
+  fc32_FilterChain_setup(&filter_chain);
+
+  const int32_t input = 100;
+  const int32_t error_tol = 1;
+  int32_t expected;
+  int32_t output = 0;
+
+  int32_t expected_outputs[] = {
+    20,
+    36,
+    49,
+    59,
+    67,
+    74,
+    79,
+    83,
+    87,
+    89,
+    91,
+    93,
+    95,
+    96,
+    96,
+    97,
+    98,
+    98,
+    99,
+  };
+
+  for (size_t i = 0; i < COUNT_OF(expected_outputs); i++)
+  {
+    expected = expected_outputs[i];
+    output = fc32_FilterChain_filter(&filter_chain, input);
+    EXPECT_NEAR(expected, output, error_tol);
+  }
+
+}
+
+
+
+TEST(FilterChain_i32, OneIirDynamicCalc) {
+
+  fcb32_IirLowPass1 iir1;
+  fcb32_IirLowPass1_new(&iir1);
+  iir1.new_ratio = 0.3f;
+
+
+  fc32_FilterChain filter_chain = { 0 };
+  fcb32_GenericBlock* blocks[] = {
+    &iir1.block,
+  };
+  filter_chain.blocks = &blocks[0];
+  filter_chain.block_count = COUNT_OF(blocks);
+  fc32_FilterChain_setup(&filter_chain);
+
+  const int32_t input = 100;
+  const int32_t error_tol = 1;
+  int32_t expected;
+  int32_t output = 0;
+  int32_t difference;
+
+  for (size_t i = 0; i < 50; i++)
+  {
+    int32_t last_output = output;
+    difference = input - last_output;
+    expected = (int32_t)(difference * iir1.new_ratio) + last_output;
+    output = fc32_FilterChain_filter(&filter_chain, input);
+    EXPECT_NEAR(expected, output, error_tol);
   }
 }
 
