a7e9e85398160a18f0badcdb8097befb1787d39c
Adam FK <adam.k@jcaelectronics.ca>
2018-03-19 21:06:14 -0500

    test: add another passthrough test

--- source/i32-filter-chain.gtest.cpp
+++ source/i32-filter-chain.gtest.cpp
@@ -16,14 +16,16 @@
 #define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
 
 
-TEST(FilterChain_i32, PassthroughTest) {
 
-  fcb32_PassThrough p32;
-  fcb32_PassThrough_new(&p32);
+
+TEST(FilterChain_i32, OnePassthrough) {
+
+  fcb32_PassThrough p;
+  fcb32_PassThrough_new(&p);
 
   fc32_FilterChain filter_chain = { 0 };
   fcb32_GenericBlock* blocks[] = {
-    &p32.block,
+    &p.block,
   };
 
   filter_chain.blocks = &blocks[0];
@@ -37,8 +39,37 @@ TEST(FilterChain_i32, PassthroughTest) {
     int32_t output = fc32_FilterChain_filter(&filter_chain, input);
     EXPECT_EQ(output, input);
   }
-
 }
+
+
+TEST(FilterChain_i32, TwoPassthrough) {
+
+  fcb32_PassThrough p1;
+  fcb32_PassThrough_new(&p1);
+
+  fcb32_PassThrough p2;
+  fcb32_PassThrough_new(&p2);
+
+
+  fc32_FilterChain filter_chain = { 0 };
+  fcb32_GenericBlock* blocks[] = {
+    &p1.block,
+    &p2.block,
+  };
+
+  filter_chain.blocks = &blocks[0];
+  filter_chain.block_count = COUNT_OF(blocks);
+  fc32_FilterChain_setup(&filter_chain);
+
+
+  for (size_t i = 0; i < 50; i++)
+  {
+    int32_t input = i;
+    int32_t output = fc32_FilterChain_filter(&filter_chain, input);
+    EXPECT_EQ(output, input);
+  }
+}
+
 
 
 int main(int argc, char** argv) {
